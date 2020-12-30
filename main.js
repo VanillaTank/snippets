7fb9e4f8e1e59d06afe3a054b213010b010bfca4
VanillaTank <tedien2016@yandex.ru>
2020-12-30 14:07:15 +0800

    клик только на мобилках

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-    //Анимация формы 
-    const shakeBlock = document.querySelector('.shakeBlock');
-    const shakeBtn = document.querySelector('.shakeBtn');
-    shakeBtn.addEventListener('click', ()=>{
-        shakeBlock.className = "shakeBlock fallingBlock";
-        shakeBlock.classList.remove('animForm-shake');
-        shakeBlock.scrollWidth = shakeBlock.scrollWidth;
-        shakeBlock.classList.add('animForm-shake');
-    });
+//Анимация формы 
+const shakeBlock = document.querySelector('.shakeBlock');
+const shakeBtn = document.querySelector('.shakeBtn');
+shakeBtn.addEventListener('click', () => {
+    shakeBlock.className = "shakeBlock fallingBlock";
+    shakeBlock.classList.remove('animForm-shake');
+    shakeBlock.scrollWidth = shakeBlock.scrollWidth;
+    shakeBlock.classList.add('animForm-shake');
+});
 
-    const fallingBtn = document.querySelector('.fallingBtn');
-    const fallingBlock = document.querySelector('.fallingBlock');
-    fallingBtn.addEventListener('click', ()=>{
-        fallingBlock.className = "shakeBlock fallingBlock";
-        fallingBlock.classList.remove('animForm-falling');
-        fallingBlock.scrollWidth = fallingBlock.scrollWidth;
-        fallingBlock.classList.add('animForm-falling')
-    });
+const fallingBtn = document.querySelector('.fallingBtn');
+const fallingBlock = document.querySelector('.fallingBlock');
+fallingBtn.addEventListener('click', () => {
+    fallingBlock.className = "shakeBlock fallingBlock";
+    fallingBlock.classList.remove('animForm-falling');
+    fallingBlock.scrollWidth = fallingBlock.scrollWidth;
+    fallingBlock.classList.add('animForm-falling')
+});
 
-    //Переворачивающиеся карточки(для телефонов)
+//Переворачивающиеся карточки(для телефонов)
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     const cards = document.querySelectorAll('.bl-1__card');
-    cards.forEach(item =>{
-        item.addEventListener('click', (evt) =>{
+    cards.forEach(item => {
+        item.addEventListener('click', (evt) => {
             let currentCard = evt.target.parentNode;
-            if(evt.target.className == "bl-1__front") {
+            if (evt.target.className == "bl-1__front") {
                 currentCard.childNodes[1].style.transform = 'rotateY(180deg)';
                 currentCard.childNodes[3].style.transform = 'rotateY(360deg)';
-            }else {
+            } else {
                 currentCard.childNodes[1].style.transform = 'rotateY(0deg)';
                 currentCard.childNodes[3].style.transform = 'rotateY(180deg)';
             }
         })
     })
+}
+
 
