f39dbb2f9d05cb31c0e2c72efac57db25c6e8a68
Tatiana Mikhailova <tedien2016@yandex.ru>
2022-04-09 23:26:07 +0800

    add rain animation 2

--- main.js
+++ main.js
@@ -474,3 +474,27 @@ function resize_photo() {
   function removeDiv() { this.remove() }
 
 })();
+
+// -------------------------------------------------------------------------------------
+// Анимация дождя 2
+(function () {
+  let quantity = 240;
+  let body = document.querySelector('.b-24 .example');
+  let index = 0;
+
+  while( index < quantity ) {
+    let drop = document.createElement('index');
+    let size = Math.random() * 5;
+    let positionX = Math.floor(Math.random() * body.clientWidth);
+    let delay = Math.random() * 20;
+    let duration = Math.random() * 5;
+
+    drop.style.width = 0.3 + size + 'px';
+    drop.style.left = positionX + 'px';
+    drop.style.animationDelay = delay + 's';
+    drop.style.animationDuration = 7 + duration + 's';
+
+    body.appendChild(drop)
+    index++;
+  }
+})();
